aa5d26b0d9459640da6cf6dac124ae0529fef9ae
Cristobal Forno <cforno12@linux.ibm.com>
2020-06-23 21:31:32 -0500

    Scrapper controller: removed comments
    Removed comments that aided in webscrapping Tunefind

--- app/controllers/scrapper_controller.rb
+++ app/controllers/scrapper_controller.rb
@@ -37,9 +37,3 @@ class ScrapperController < ApplicationController
     render template: 'scrapper/run'
   end  
 end
-
-#song title:
-#<a href="/song/142232/190022/" class="SongTitle__link___2OQHD" title="Gap in the Clouds">Gap in the Clouds</a>
-#song artist:
-#<a href="/artist/yung-bans" class="Subtitle__subtitle___1rSyh">Yung Bans</a>
-
